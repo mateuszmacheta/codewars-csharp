7d0d9c84d9f73365c3c55ccaa1cc31abbc69b640
Mateusz Macheta <mateusz.macheta@ts.fujitsu.com>
2020-09-30 16:23:53 +0200

    workin on simple square numbers - continued

--- simple-square-numbers/Program.cs
+++ simple-square-numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace simple_square_numbers
 {
@@ -6,19 +7,54 @@ namespace simple_square_numbers
     {
         public static long solve(long n)
         {
+            //Console.WriteLine(n);
             if (n <= 1) { return -1; }
-            double test = (double)(n - 1) / 2;
-            if (test % 1 == 0)
-            { return (long)Math.Pow(test, 2); }
+            long sqr;
+
+            // for (long i = n; ; i /= 2)
+            // {
+            //     System.Console.WriteLine(n / i);
+            // }
+
+            // for (int i = 2; n / i >= 1; i *= 2)
+            // {
+            //     sqr = (long)Math.Pow(n / i, 2);
+            //     System.Console.WriteLine($"i\t{i}\tsqr\t{sqr}");
+            //     if (Math.Sqrt(n + sqr) % 1 == 0)
+
+            //     { return sqr; }
+            // }
+
+            // for (int i = 1; i < n / 4 + 1; i++)
+            // {
+            //     sqr = (long)Math.Pow(i, 2);
+            //     System.Console.WriteLine($"i\t{i}\tsqr\t{sqr}");
+            //     if (Math.Sqrt(n + sqr) % 1 == 0)
+            //     { return sqr; }
+            // }
+            // if (Math.Sqrt(n + (long)Math.Pow(n / 2, 2)) % 1 == 0)
+            // { return (long)Math.Pow(n / 2, 2); }
+            // if (Math.Sqrt(n + (long)Math.Pow(n / 2 + 1, 2)) % 1 == 0)
+            // { return (long)Math.Pow(n / 2 + 1, 2); }
             return -1;
         }
         static void Main(string[] args)
         {
-            var test = new long[] { 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 17, 88901, 290101 };
-            foreach (var item in test)
-            {
-                Console.WriteLine("{0} e: {1}", item, solve(item));
-            }
+            var test = Enumerable.Range(1, 17).Select(x => (long)x).ToArray(); ;
+
+            System.Console.WriteLine(solve(5));
+            // Console.WriteLine("n\te");
+            // foreach (var item in test)
+            // {
+            //     Console.WriteLine("{0}\t{1}", item, solve(item));
+            // }
+            // using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\machetam\Desktop\out.txt"))
+            // {
+            //     foreach (var item in test)
+            //     {
+            //         file.WriteLine(string.Format("{0}\t{1}", item, solve(item)));
+            //     }
+            // }
 
         }
     }
