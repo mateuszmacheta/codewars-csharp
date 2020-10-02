bdcb01b70aa7ba8187b8a2db2f84ad58c2c9ca89
Mateusz Macheta <mateusz.macheta@ts.fujitsu.com>
2020-10-02 17:15:42 +0200

    simple square numbers passed

--- simple-square-numbers/Program.cs
+++ simple-square-numbers/Program.cs
@@ -11,38 +11,20 @@ namespace simple_square_numbers
             if (n <= 1) { return -1; }
             long sqr;
 
-            // for (long i = n; ; i /= 2)
-            // {
-            //     System.Console.WriteLine(n / i);
-            // }
+            for (int i = (int)Math.Log(n, 2); i >= 1; i--)
+            {
+                sqr = (long)Math.Pow(n / Math.Pow(2, i), 2);
+                if (Math.Sqrt(n + sqr) % 1 == 0)
+                { return sqr; }
+            }
 
-            // for (int i = 2; n / i >= 1; i *= 2)
-            // {
-            //     sqr = (long)Math.Pow(n / i, 2);
-            //     System.Console.WriteLine($"i\t{i}\tsqr\t{sqr}");
-            //     if (Math.Sqrt(n + sqr) % 1 == 0)
-
-            //     { return sqr; }
-            // }
-
-            // for (int i = 1; i < n / 4 + 1; i++)
-            // {
-            //     sqr = (long)Math.Pow(i, 2);
-            //     System.Console.WriteLine($"i\t{i}\tsqr\t{sqr}");
-            //     if (Math.Sqrt(n + sqr) % 1 == 0)
-            //     { return sqr; }
-            // }
-            // if (Math.Sqrt(n + (long)Math.Pow(n / 2, 2)) % 1 == 0)
-            // { return (long)Math.Pow(n / 2, 2); }
-            // if (Math.Sqrt(n + (long)Math.Pow(n / 2 + 1, 2)) % 1 == 0)
-            // { return (long)Math.Pow(n / 2 + 1, 2); }
             return -1;
         }
         static void Main(string[] args)
         {
             var test = Enumerable.Range(1, 17).Select(x => (long)x).ToArray(); ;
 
-            System.Console.WriteLine(solve(5));
+            System.Console.WriteLine(solve(3));
             // Console.WriteLine("n\te");
             // foreach (var item in test)
             // {
--- test/Program.cs
+++ test/Program.cs
@@ -1,26 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace test
 {
     class Program
     {
-        public static (int, int) solve(int s, int g)
+        public string ReverseLetter(string str)
         {
-            int num = g; int num2;
-            while (num < s)
-            {
-                num2 = s - num;
-                if ((float)num2 / g % 1 == 0)
-                { return num <= num2 ? (num, num2) : (num2, num); }
-                num += g;
-            }
-            return (-1, -1);
+              return string.Join("",str.Select(c => Char.IsLetter(c)).Reverse());
         }
 
         static void Main(string[] args)
         {
             //var test = new List<int> { 15, 11, 10, 7, 8 };
-            System.Console.WriteLine(solve(10, 3));
+            System.Console.WriteLine(MatrixAddition(new int[][] {new int[] {1, 2},
+                                                                                       new int[] {1, 2}},
+                                                                                       new int[][] {new int[] {2, 3},
+                                                                                                    new int[] {2, 3}}));
         }
 
     }
