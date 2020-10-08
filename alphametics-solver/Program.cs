7546fe310e233353bba78ca0f5682f2b33ba2583
Mateusz Macheta <mateusz.macheta@wp.pl>
2020-10-08 20:21:30 +0200

    alphametics

---
+++ alphametics-solver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace alphametics_solver
+{
+    class Program
+    {
+
+        public static string Alphametics(string s)
+        {
+            char[] chars = s.Where(c => Char.IsLetter(c)).Distinct().ToArray();
+            return "";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Alphametics("SEND + MORE = MONEY"));
+        }
+    }
+}
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -5,24 +5,29 @@ namespace codewars_sandbox
 {
     class Program
     {
-        public static bool validBraces(String braces)
+        public static bool isTriangleNumber(long x)
         {
-            int bLen = braces.Length;
-            string[] pairs = new string[] { "()", "{}", "[]" };
-            do
-            {
-                bLen = braces.Length;
-                foreach (var pair in pairs)
-                {
-                    braces = braces.Replace(pair, "");
-                }
-            } while (bLen != braces.Length);
-            return braces == "";
+            if (x == 0 || x == 1) { return true; }
+
+            // need to find solution of this equation: n^2 + n -2x = 0
+            // and the solution has to be a whole number
+            // notice that there is only one positive solution, so we ignore the second one
+            // the solution we search for is n_1 = [-1 - (1+8x)^1/2]/2 where 1+8x is our delta
+
+            // let's check if root delta is a whole number
+            long delta = 1 + 8 * x;
+            double deltaSqrt = Math.Sqrt(delta);
+
+            if (!(deltaSqrt % 1 == 0)) { return false; }
+
+            // now we check if n_1 is a whole number
+            return ((-1 - (long)deltaSqrt) / 2 % 1 == 0);
+
         }
         static void Main(string[] args)
         {
 
-            System.Console.WriteLine(validBraces("[]()"));
+            System.Console.WriteLine(isTriangleNumber(125250));
         }
     }
 }
