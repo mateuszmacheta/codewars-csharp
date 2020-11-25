588b0d8853b50c0e706eabed8fcc54288c88455f
Macheta <mateusz.macheta@fujitsu.com>
2020-11-25 23:15:41 +0100

    Remove ignored files

---
+++ scrambies/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Scramblies
+{
+
+    public static bool Scramble(string str1, string str2)
+    {
+        var mainList = str1.ToList();
+        var subList = str2.ToList();
+        bool isRemoved;
+
+        foreach (var item in subList)
+        {
+            isRemoved = mainList.Remove(item);
+            if (!isRemoved)
+                return false;
+        }
+
+        return true;
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Scramblies.Scramble("katas", "steak"));
+    }
+}
+
