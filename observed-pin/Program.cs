4a6980627577c42c653e8290f6759942b052d3da
Mateusz Macheta <mateusz.macheta@wp.pl>
2021-02-10 22:29:45 +0100

    adding big numbers

---
+++ observed-pin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace observed_pin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sum-big-numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+
+public class Kata
+{
+    public static string Add(string a, string b)
+    {
+        BigInteger x = 0;
+        BigInteger y = 0;
+        x = BigInteger.Parse(a);
+        y = BigInteger.Parse(b);
+
+        return Convert.ToString(x + y).TrimStart('0');
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
