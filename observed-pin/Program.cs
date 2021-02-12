fda6aad9cfbb5c915a8f2a0db0f44c15560bd359
Mateusz Macheta <mateusz.macheta@fujitsu.com>
2021-02-12 07:55:47 +0100

    observed pin is working

--- observed-pin/Program.cs
+++ observed-pin/Program.cs
@@ -5,12 +5,12 @@ using System.Text;
 
 public class Kata
 {
-    private static List<string> getAllPermuations(List<List<char>> input)
+    private static List<string> getAllPermuations(List<string> input)
     {
         var output = new List<string>();
         StringBuilder sb = new StringBuilder();
         int[] register = new int[input.Count];
-        int permutationCount = input.Aggregate(1, (x, y) => x * y.Count);
+        int permutationCount = input.Aggregate(1, (x, y) => x * y.Length);
         int carry;
         int limit;
         for (int i = 0; i < permutationCount; i++) // make all required permutations
@@ -18,10 +18,8 @@ public class Kata
             carry = 1;
             for (int j = 0; j < input.Count; j++) // iterate through all lists
             {
-                //System.Console.Write($"{i}: ");
-                //System.Console.WriteLine(register.Aggregate("", (x, y) => x + y.ToString()));
                 sb.Append(input[j][register[j]]);
-                limit = input[j].Count;
+                limit = input[j].Length;
                 register[j] += carry;
                 if (register[j] >= limit)
                 {
@@ -43,19 +41,19 @@ public class Kata
 
     public static List<string> GetPINs(string observed)
     {
-        var allDigits = new Dictionary<char, List<char>>(){
-            {'0',new List<char>(){'0','8'}},
-            {'1', new List<char>(){'1','2','4'}},
-            {'2',new List<char>(){'1','2','3','5'}},
-            {'3',new List<char>(){'2','3','6'}},
-            {'4',new List<char>(){'1','4','5','7'}},
-            {'5',new List<char>(){'2','4','5','6','8'}},
-            {'6',new List<char>(){'3','5','6','9'}},
-            {'7',new List<char>(){'4','7','8'}},
-            {'8',new List<char>(){'5','7','8','9','0'}},
-            {'9',new List<char>(){'6','8','9'}}
+        var allDigits = new Dictionary<char, string>(){
+            {'0', "08"},
+            {'1', "124"},
+            {'2',"1235"},
+            {'3',"236"},
+            {'4',"1457"},
+            {'5',"24568"},
+            {'6',"3569"},
+            {'7',"478"},
+            {'8',"05789"},
+            {'9',"689"}
         };
-        List<List<char>> input = new List<List<char>>();
+        List<string> input = new List<string>();
         foreach (var c in observed)
         {
             input.Add(allDigits[c]);
