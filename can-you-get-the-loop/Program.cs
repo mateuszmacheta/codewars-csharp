49af1eb2989009df538ea308bae84c6737a9efb6
Mateusz Macheta <mateusz.macheta@fujitsu.com>
2021-02-12 07:16:20 +0100

    working on can you find the loop

---
+++ can-you-get-the-loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface Node
+{
+    Node next();
+}
+public class Kata
+{
+    public static int getLoopSize(Node startNode)
+    {
+        List<Node> seen = new List<Node>();
+        Node currentNode;
+        int counter = 0;
+        int seenIndex;
+        currentNode = startNode;
+        seen.Add(currentNode);
+        while ((seenIndex = seen.IndexOf(currentNode)) >= 0)
+        {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return counter - seenIndex;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
