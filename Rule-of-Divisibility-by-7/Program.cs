d2ab3e365ccae80cc5a78293a0af549ef661c84d
Mateusz Macheta <mateusz.macheta@wp.pl>
2021-03-08 20:47:48 +0100

    divisibility by 7

---
+++ Rule-of-Divisibility-by-7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Rule_of_Divisibility_by_7
+{
+    public class DivSeven
+    {
+
+        public static long[] Seven(long m)
+        {
+            int steps = 0; long clipped, lastDigit;
+            while (Math.Log10(m) >= 2)
+            {
+                clipped = m / 10;
+                lastDigit = m % 10;
+                m = clipped - lastDigit * 2;
+                steps++;
+            }
+
+            return new long[] { steps, m };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] output = DivSeven.Seven(371);
+            Console.WriteLine(output[0]);
+            Console.WriteLine(output[1]);
+        }
+    }
+}
