13c31a2d14399b70332c732f3a89a602965c6120
Mateusz Macheta <mateusz.macheta@wp.pl>
2022-02-17 16:24:11 +0100

    working on twice linear

--- base64/Program.cs
+++ base64/Program.cs
@@ -7,48 +7,81 @@ public static class Base64Utils
 	public static string ToBase64(string s)
 	{
 		StringBuilder sb = new StringBuilder();
-
 		var bytes = Encoding.UTF8.GetBytes(s);
 
 		for (int i = 0; i < bytes.Length; i += 3)
 		{
-			System.Console.Write($"i: {i} ");
-			System.Console.Write($"bytes[{i}]: {Convert.ToString(bytes[i], 2).PadLeft(8, '0')}");
-			if (i + 1 < bytes.Length)
-				System.Console.Write($" bytes[{i + 1}]: {Convert.ToString(bytes[i + 1], 2).PadLeft(8, '0')}");
-			if (i + 2 < bytes.Length)
-				System.Console.Write($" bytes[{i + 2}]: {Convert.ToString(bytes[i + 2], 2).PadLeft(8, '0')}");
-			System.Console.WriteLine();
-
-			int word = 0b_0000_000_0000_0000_0000_0000_0000_0000;
+			int word = 0;
+			char[] encodedChars = new char[] { '=', '=', '=', '=' };
 
 			// combine three bytes to form a 24-bit word
-
 			word += bytes[i] << 16;
-			word += bytes[i + 1] << 8;
-			word += bytes[i + 2];
+			if (i + 1 < bytes.Length)
+				word += bytes[i + 1] << 8;
+			if (i + 2 < bytes.Length)
+				word += bytes[i + 2];
 
 			// split then into 4 6-bit parts and translate
+			encodedChars[0] = base64table[word >> 18];
+			encodedChars[1] = base64table[(word & 0b_0000_0000_0000_0011_1111_0000_0000_0000) >> 12];
+			if (i + 1 < bytes.Length)
+				encodedChars[2] = base64table[(word & 0b_0000_0000_0000_0000_0000_1111_1100_0000) >> 6];
+			if (i + 2 < bytes.Length)
+				encodedChars[3] = base64table[word & 0b_0000_0000_0000_0000_0000_0000_0011_1111];
 
-			sb.Append(base64table[word >> 18]);
-			sb.Append(base64table[word & 0b_0000_000_0000_0011_1111_0000_0000_0000 >> 12]);
-			sb.Append(base64table[word & 0b_0000_000_0000_0000_0000_1111_1100_0000 >> 6]);
-			sb.Append(base64table[word & 0b_0000_000_0000_0000_0000_0000_0011_1111]);
+			sb.Append(string.Join("", encodedChars));
 		}
-
-		return s;
+		return sb.ToString();
 	}
 
 	public static string FromBase64(string s)
 	{
-		// Happy coding!
-		return s;
+		if (s.Length % 4 != 0)
+			throw new SystemException("Input string has incorrect length.");
+		if (s.Length == 0)
+			return "";
+
+		StringBuilder sb = new StringBuilder();
+		char[] decodedChars = new char[3];
+		int i = 0; int word = 0;
+
+		for (; i < s.Length - 4; i += 4)
+		{
+			word = 0;
+			word += Array.IndexOf(base64table, s[i]) << 18;
+			word += Array.IndexOf(base64table, s[i + 1]) << 12;
+			word += Array.IndexOf(base64table, s[i + 2]) << 6;
+			word += Array.IndexOf(base64table, s[i + 3]);
+
+			sb.Append((char)(word >> 16));
+			sb.Append((char)((word & 0b_0000_0000_0000_0000_1111_1111_0000_0000) >> 8));
+			sb.Append((char)(word & 0b_0000_0000_0000_0000_0000_0000_1111_1111));
+		}
+
+		word = 0;
+		word += Array.IndexOf(base64table, s[i]) << 18;
+		word += Array.IndexOf(base64table, s[i + 1]) << 12;
+		sb.Append((char)(word >> 16));
+
+		if (s.EndsWith("=="))
+			return sb.ToString();
+
+		word += Array.IndexOf(base64table, s[i + 2]) << 6;
+		sb.Append((char)((word & 0b_0000_0000_0000_0000_1111_1111_0000_0000) >> 8));
+
+		if (s.EndsWith("="))
+			return sb.ToString();
+
+		word += Array.IndexOf(base64table, s[i + 3]);
+		sb.Append((char)(word & 0b_0000_0000_0000_0000_0000_0000_1111_1111));
+		return sb.ToString();
 	}
 }
 class Program
 {
 	static void Main(string[] args)
 	{
-		System.Console.WriteLine(Base64Utils.ToBase64("Many hands make light work."));
+		System.Console.WriteLine(Base64Utils.ToBase64("M"));
+		System.Console.WriteLine(Base64Utils.FromBase64("bGlnaHQgd29y"));
 	}
 }
---
+++ twice-linear/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace twice_linear
+{
+	public class DoubleLinear
+	{
+		private static HashSet<int> a = new HashSet<int>() { 1 };
+
+		static DoubleLinear()
+		{
+			const int max = 60000;
+
+			int x = 0, y = 0;
+
+			for (int i = 0; i < max; i++)
+			{
+				int nextX = 2 * a.ElementAt(i) + 1;
+				int nextY = 3 * a.ElementAt(i) + 1;
+				if (nextX <= nextY)
+				{
+					a.Add(nextX);
+					x++;
+					if (nextX == nextY) y++;
+				}
+				else
+				{
+					a.Add(nextY);
+					y++;
+				}
+			}
+		}
+
+		public static int DblLinear(int n)
+		{
+			System.Console.WriteLine($"n: {n}");
+			return a.ElementAt(n);
+		}
+	}
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			var watch = System.Diagnostics.Stopwatch.StartNew();
+			Console.WriteLine(DoubleLinear.DblLinear(10));
+			System.Console.WriteLine($"Time: {watch.ElapsedMilliseconds / 1000:D2} s");
+		}
+	}
+}
